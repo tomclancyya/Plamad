187228ee8038f2b6cf28a9980f1b462a3bca9090
RV-Savelich <t89086460461@gmail.com>
2022-04-12 16:38:58 +0800

    Update timer-test.js

--- test/utils/timer-test.js
+++ test/utils/timer-test.js
@@ -10,25 +10,43 @@ export class TimerTest {
             "timer should":
             {
                 "stop the countdown in 5 sec": () => {
-                    let timer = new Timer (5000)
+                    let timer = new Timer (5000)                    
+                    console.log(timer.currentTime/1000);
+                    console.log('isFinished =', timer.isFinished());
                     timer.update (1000); 
+                    console.log(timer.currentTime/1000);
+                    console.log('isFinished =', timer.isFinished());
                     timer.update (1000); 
+                    console.log(timer.currentTime/1000);
+                    console.log('isFinished =', timer.isFinished());
                     timer.update (1000); 
+                    console.log(timer.currentTime/1000);
+                    console.log('isFinished =', timer.isFinished());
                     timer.update (1000); 
+                    console.log(timer.currentTime/1000);
+                    console.log('isFinished =', timer.isFinished());
                     timer.update (1000); 
-                    timer.update (1000); 
-                    return isBooleanEqual(timer.isFinished(),true)
-   
+                    console.log(timer.currentTime/1000);
+                    console.log('isFinished =', timer.isFinished());
+                    return isBooleanEqual(timer.isFinished(),true)   
                 },
-                "return the starting value": () => {
+                "return true when the value of current time = 0": () => {
+                    let timer = new Timer (0)                                                                                             
+                    return isNumberEqual(timer.currentTime, 0)
+                },
+
+                "get the initial value after reset": () => {
                     let timer = new Timer (5000)
-                    timer.update (1000); 
                     timer.update (1000); 
                     timer.update (1000); 
                     timer.update (1000); 
                     timer.update (1000); 
                     timer.update (1000);                     
-                    timer.reset()                                                               
+                    console.log();
+                    console.log('Current time = ', timer.currentTime);
+                    console.log('isFinished with the current time = ', timer.isFinished());                                
+                    timer.reset()    
+                    console.log('isFinished after reset = ', timer.isFinished());                                        
                     return isNumberEqual(timer.currentTime, 5000)
                 }
                 
