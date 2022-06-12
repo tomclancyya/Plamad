3ecf1dd6d820d9012a91a764db68eaeea4882832
RV-Savelich <t89086460461@gmail.com>
2022-06-12 17:42:57 +0800

    Update ticker-test.js

--- test/test-engine/ticker-test.js
+++ test/test-engine/ticker-test.js
@@ -1,5 +1,5 @@
 import { Ticker, TickerSettings } from "../../engine/ticker";
-import { done } from "./test-assertion";
+import { done, isBooleanEqual, isNumberEqual } from "./test-assertion";
 
 
 export class TickerTest {
@@ -8,33 +8,107 @@ export class TickerTest {
         return {
             "ticker should":
             {
-                "tick 5 times and stop": () => {                    
-                    function sayHi() {
-                        console.log("Привет");
-                      }
-                    let settings = new TickerSettings
-                    settings.tickPerSeconds = 1
-                    settings.tickerTimeLimitSec = 5
-                    let ticker = new Ticker(settings, sayHi); 
-                    let int = new IntervalMock()   
-                    ticker.interval = int.sayHi();
-                    console.log(ticker.interval);
+                // "tick 3 times and stop by calling clearInterval": () => {                    
+                //     function blankFunction() {
+                //         // return undefined;
+                //     }
 
-                    return done();   
+                //     let tickerSettings = new TickerSettings()
+                //     tickerSettings.tickPerSeconds = 1
+                //     tickerSettings.tickerTimeLimitSec = 3
+                    
+                //     let tick = null  
+                //     Ticker.prototype.setInterval = function (callback, delta) {
+                //         tick = callback
+                //     }
+
+                //     let stop = false
+                //     Ticker.prototype.clearInterval = function () {
+                //         stop = true
+                //     }
+
+                //     let ticker = new Ticker(tickerSettings, blankFunction);                     
+                //     isNumberEqual(ticker.ticks, 0)
+                //     isBooleanEqual(stop, false)
+                //     tick()
+                //     isNumberEqual(ticker.ticks, 1)
+                //     isBooleanEqual(stop, false)
+                //     tick()
+                //     isNumberEqual(ticker.ticks, 2)
+                //     isBooleanEqual(stop, false)
+                //     tick()
+                //     isNumberEqual(ticker.ticks, 3)
+                //     isBooleanEqual(stop, false)
+                //     tick()
+                //     isNumberEqual(ticker.ticks, 0)    
+                //     isBooleanEqual(stop, true)         
+                //     return done();   
+                // },
+                // "click on counter in the check function": () => {                    
+                //     let counter = 0
+                //     function checkFunction() {                        
+                //         counter++                                                            
+                //     }
+
+                //     let tickerSettings = new TickerSettings()
+                //     tickerSettings.tickPerSeconds = 1
+                //     tickerSettings.tickerTimeLimitSec = 2 //можно убрать, похуй
+
+                //     let tick = null  
+                //     Ticker.prototype.setInterval = function (callback, delta) {
+                //         tick = callback
+                //     }
+                                                   
+                //     let ticker = new Ticker(tickerSettings, checkFunction);                      
+                //     tick()                                    
+                //     return isNumberEqual(counter, 1);                                                          
+                // },                
+                // "infinity": () => {                    
+                //     let counter = 0
+                //     function checkFunction() {                        
+                //         counter++    
+                //         console.log(counter);                                                        
+                //     }
+
+                //     let tickerSettings = new TickerSettings()
+                //     tickerSettings.tickPerSeconds = 1
+                //     tickerSettings.tickerTimeLimitSec = 1
+
+                //     let tick = null  
+                //     Ticker.prototype.setInterval = function (callback, delta) {
+                //         tick = callback
+                //     }
+                                                   
+                //     let ticker = new Ticker(tickerSettings, checkFunction);                      
+                //     tick()   
+                //     tick()
+                //     tick()
+                //     tick()                                 
+                //     return done();
+                // },
+                "where is delta value": () => {                
+                    
+                    function blankFunction(delta) {                        
+                        console.log(delta);                                                
+                    }
+
+                    let tickerSettings = new TickerSettings()
+                    tickerSettings.tickPerSeconds = 1
+                    tickerSettings.tickerTimeLimitSec = 10  
+                    
+                    tickerSettings.ticker
+                                                   
+                    let ticker = new Ticker(tickerSettings, blankFunction);     
+                    
+                    
+                    //isNumberEqual(ticker.ticks, 6)
+                                          
+                    return done();
                 }
-                
+
             
             } 
         }
     }
 }
 
-class IntervalMock {
-    constructor() { }
-
-    sayHi() {
-        console.log('ее');
-      }
-
-      
-}
