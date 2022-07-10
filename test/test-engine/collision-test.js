d4196d5ecb47221025a1bbbd1d34e9c991925747
RV-Savelich <102396772+RV-Savelich@users.noreply.github.com>
2022-07-10 10:23:21 +0800

    Update collision-test.js

--- test/test-engine/collision-test.js
+++ test/test-engine/collision-test.js
@@ -10,8 +10,7 @@ export class CollisionTest {
     getTests() {
         return {
             "collision test should.only":
-            {"should delete the smaller planet when different planets have meet": () => {                                
-                console.log();
+            {"should delete the smaller planet when different planets have meet": () => {                                                
                 let scene = new Scene (1000)
 
                 let position1 = new Vector2 (50, 50)
@@ -39,13 +38,80 @@ export class CollisionTest {
                     smallest = planet1
                   } else {
                     smallest = planet2
-                  }                    
+                  } 
+                  
+                isBooleanEqual(smallest.isActive, true);   
                
                 let collision = new CollisionEngine (scene)                     
                 collision.checkPlanetsCollision ()      
                 
                 return isBooleanEqual(smallest.isActive, false)   
-            }            
+            },
+            "should not delete any planet when the same planets have meet": () => {                                                
+                let scene = new Scene (1000)
+
+                let position1 = new Vector2 (50, 50)
+                let planetView1 = {
+                    setSize: function(){},
+                    setGrayColor: function(){}
+                }// new PlanetView(0, 0, 100, 'text', '0x6699ff', 'gameplay');
+                let planet1 = new Planet (planetView1, scene, null, 'planet1');
+                planet1.moveToPosition(position1)
+                planet1.level = 1
+                planet1.applyLevelStats()
+
+                let position2 = new Vector2 (50, 50)
+                let planetView2 = {
+                    setSize: function(){},
+                    setGrayColor: function(){}
+                }// new PlanetView(0, 0, 100, 'text', '0x6699ff', 'gameplay');
+                let planet2 = new Planet (planetView2, scene, null, 'planet2');
+                planet2.moveToPosition(position2)
+                planet2.level = 1
+                planet2.applyLevelStats()                                    
+                  
+                isBooleanEqual(planet1.isActive, true)
+                isBooleanEqual(planet2.isActive, true)   
+               
+                let collision = new CollisionEngine (scene)                     
+                collision.checkPlanetsCollision ()      
+                
+                isBooleanEqual(planet1.isActive, true)
+                return isBooleanEqual(planet2.isActive, true)                  
+            },
+            "should not delete any planet when the same planets have meet": () => {                                                
+                let scene = new Scene (1000)
+
+                let position1 = new Vector2 (50, 50)
+                let planetView1 = {
+                    setSize: function(){},
+                    setGrayColor: function(){}
+                }// new PlanetView(0, 0, 100, 'text', '0x6699ff', 'gameplay');
+                let planet1 = new Planet (planetView1, scene, null, 'planet1');
+                planet1.moveToPosition(position1)
+                planet1.level = 1
+                planet1.applyLevelStats()
+
+                let position2 = new Vector2 (50, 50)
+                let planetView2 = {
+                    setSize: function(){},
+                    setGrayColor: function(){}
+                }// new PlanetView(0, 0, 100, 'text', '0x6699ff', 'gameplay');
+                let planet2 = new Planet (planetView2, scene, null, 'planet2');
+                planet2.moveToPosition(position2)
+                planet2.level = 1
+                planet2.applyLevelStats()                                    
+                  
+                isBooleanEqual(planet1.isActive, true)
+                isBooleanEqual(planet2.isActive, true)   
+               
+                let collision = new CollisionEngine (scene)                     
+                collision.checkPlanetsCollision ()      
+                
+                isBooleanEqual(planet1.isActive, true)
+                return isBooleanEqual(planet2.isActive, true)                  
+            }        
+            
             }              
         }
     }
